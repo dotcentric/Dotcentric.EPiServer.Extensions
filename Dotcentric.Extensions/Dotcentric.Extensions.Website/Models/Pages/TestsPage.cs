2054ea4bd267cbb421edc3aeed3284721e555623
Giuliano Dore <giuliano@gogetgroomed.co.uk>
2021-03-07 19:05:45 +0000

    added tests page

---
+++ Dotcentric.Extensions/Dotcentric.Extensions.Website/Models/Pages/TestsPage.cs
@@ -0,0 +1,14 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace Dotcentric.Extensions.Website.Models.Pages
+{
+    [ContentType(DisplayName = "Tests Page", 
+        GUID = "125d790f-9342-44cd-be7a-f6aa10d7592d", 
+        Description = "Page used to test extensions methods")]
+    public class TestsPage : PageData
+    {
+        
+    }
+}
