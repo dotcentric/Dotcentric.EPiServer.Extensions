bb043f6c1232984dd1ff5c164f2815f0b9e7d02f
Giuliano Dore <giuliano@gogetgroomed.co.uk>
2021-03-07 18:31:30 +0000

    remove the code as namespace was bringing conflicts

--- Dotcentric.EPiServer.Extensions/Dotcentric.EPiServer.Extensions.Website/Models/Pages/IHasRelatedContent.cs
+++
@@ -1,9 +0,0 @@
-using EPiServer.Core;
-
-namespace Dotcentric.EPiServer.Extensions.Website.Models.Pages
-{
-    public interface IHasRelatedContent
-    {
-        ContentArea RelatedContentArea { get; }
-    }
-}
--- Dotcentric.EPiServer.Extensions/Dotcentric.EPiServer.Extensions.Website/Models/Pages/ISearchPage.cs
+++
@@ -1,9 +0,0 @@
-namespace Dotcentric.EPiServer.Extensions.Website.Models.Pages
-{
-    /// <summary>
-    /// Marker interface for search implementation
-    /// </summary>
-    public interface ISearchPage
-    {
-    }
-}
