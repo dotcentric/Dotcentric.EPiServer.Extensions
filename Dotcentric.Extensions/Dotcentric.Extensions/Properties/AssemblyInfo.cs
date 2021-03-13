09dfcbcb3a8f1d11889369e2593d301466e0c983
Giuliano Dore <giuliano@gogetgroomed.co.uk>
2021-03-13 11:37:24 +0000

    assembly info update

--- Dotcentric.Extensions/Dotcentric.Extensions/Properties/AssemblyInfo.cs
+++ Dotcentric.Extensions/Dotcentric.Extensions/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dotcentric.Extensions")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Dotcentric EPiServer Extensions")]
+[assembly: AssemblyDescription("A set of helper functions used to extend EPiServer native functions")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Dotcentric")]
 [assembly: AssemblyProduct("Dotcentric.Extensions")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
